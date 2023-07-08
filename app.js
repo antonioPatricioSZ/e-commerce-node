c111f6d6bdff3e247c47d297d2bf5cdbbff58f49
PatricioSouza <patriciozebra3@gmail.com>
2023-07-08 11:13:25 -0300

    teste10

--- app.js
+++ app.js
@@ -13,23 +13,25 @@ import mongoSanitaze from "express-mongo-sanitize";
 const app = express()
 const port = process.env.PORT || 5000
 
-app.set("trust proxy", 1)
-app.use(rateLimiter({
-   windowMs: 15 * 60 * 1000,
-   max: 60
-}))
+// app.set("trust proxy", 1)
+// app.use(rateLimiter({
+//    windowMs: 15 * 60 * 1000,
+//    max: 60
+// }))
 
-app.use(helmet())
+//app.use(helmet())
 app.use(cors())
-app.use(mongoSanitaze())
+//app.use(mongoSanitaze())
 
-app.use(express.json())
+app.use(express.json( { limit: "10mb" } ))
 app.use(cookieParser(process.env.JWT_SECRET))
 
 app.use(express.static("./public"))
 app.use(fileUpload())
 
 app.use("/api/v1", routesController)
+
+//NODE_TLS_REJECT_UNAUTHORIZED='0' node app.js
 
 const start = async () => {
    try {
