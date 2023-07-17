16ffcd39f1f9a8644e4dc1842e0517fb1d443a71
PatricioSouza <patriciozebra3@gmail.com>
2023-07-17 20:18:06 -0300

    updated

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const port = process.env.PORT || 5000
 //    max: 60
 // }))
 
-//app.use(helmet())
-app.use(cors())
+app.use(cors({ origin: "*" }))
+app.use(helmet())
 //app.use(mongoSanitaze())
 
 app.use(express.json( { limit: "10mb" } ))
